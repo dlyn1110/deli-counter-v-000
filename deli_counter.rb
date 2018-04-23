019f56aaed73d730b4e680ff9b54f27932acdd5e
Dani Lyn <dlyn1110@gmail.com>
2018-04-23 16:24:27 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,10 +15,10 @@ end
   end
 
 def now_serving(queue)
-  if queue.length < 0 
+  if queue.length < 0
     puts "Currently serving #{queue[0]}."
     queue.shift
-  else 
+  else
     puts "There is nobody waiting to be served!"
-end
+ end
 end
