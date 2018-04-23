dac51b32a4d845aeabb51cfeeb52aff2491a4f51
Dani Lyn <dlyn1110@gmail.com>
2018-04-23 16:25:20 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,13 +1,16 @@
 
 def line(queue)
   if queue.length > 0
-     current = "The line is currently:"
-     queue.each_with_index { |name,index| current += "#{index+1} #{name}" }
+    current = "The line is currently:"
+    queue.each_with_index { |name, index|
+      current += " #{index+1}. #{name}"
+    }
     puts current
-   else
-     puts "The line is currently empty."
+  else
+    puts "The line is currently empty."
   end
 end
+
 
 def take_a_number(queue,name)
   queue.push(name)
