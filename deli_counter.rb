f65426da22724873ee2f53b893b1c1796611bc51
Dani Lyn <dlyn1110@gmail.com>
2018-04-23 16:14:23 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,9 +2,10 @@
 def line(queue)
   if queue.length > 0
      current = "The line is currently:"
-   queue.each_with_index { |name,index| current += "#{index + 1}, #{name}" }
+     queue.each_with_index { |name,index| current += "#{index+1}, #{name}" }
     puts current
-   else "The line is currently empty."
+   else
+     puts "The line is currently empty."
   end
 end
 
